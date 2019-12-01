0b73dde463fd559e93041c5b3d52489a004b18c5
honeydew <adiwiguna.git@gmail.com>
2019-12-01 17:24:53 +0700

    first commit!

---
+++ application/hello.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Muhlis Adiwiguna',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'April 20, 2019'
+	},
+
+	{
+		'author': 'Gugun',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'April 28, 2019'
+	},
+
+	{
+		'author': 'Ade',
+		'title': 'Blog Post 3',
+		'content': 'Third post content',
+		'date_posted': 'May 2, 2019'
+	}
+
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About this website')
+
+if __name__ == '__main__':
+	app.run(debug=True)
