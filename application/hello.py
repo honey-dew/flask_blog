27d408a3e38d3b5301ceff57735051a8a1059a88
honey-dew <adiwiguna.git@gmail.com>
2019-12-04 16:50:37 +0700

    update requirements.txt

--- application/hello.py
+++ application/hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '226859cfa8c9f7bebe3ad36648f61ab2'
 
 posts = [
 	{
@@ -34,6 +37,17 @@ def home():
 @app.route('/about')
 def about():
 	return render_template('about.html', title='About this website')
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+	form = RegistrationForm()
+	return render_template('Login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
